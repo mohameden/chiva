215c1e27d35907745552305bd8518af8ea862773
mohameden <k.mohameden@gmail.com>
2013-05-01 13:48:39 +0200

    #1 [Technical] add transactions

--- clinique/src/main/java/clinique/metier/securite/AuthentificationBO.java
+++ clinique/src/main/java/clinique/metier/securite/AuthentificationBO.java
@@ -1,17 +1,53 @@
 package clinique.metier.securite;
 
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionMessages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import clinique.dao.UserDAO;
 import clinique.mapping.User;
 import clinique.model.securite.SecuriteForm;
 
-public interface AuthentificationBO {
-	String NAME = "AuthentificationBO";
+@Service(AuthentificationBO.NAME)
+public class AuthentificationBOImpl implements AuthentificationBO {
+	@Autowired
+	private UserDAO userDAO;
+	private static Logger log = Logger.getLogger(AuthentificationBOImpl.class);
 
-	public User getUser(SecuriteForm formulaire) throws Exception;
+	private ActionMessages errors = new ActionMessages();
 
-	public void create(User user);
+	public ActionMessages getErrors() {
+		return errors;
+	}
 
-	ActionMessages getErrors();
+	public void setErrors(ActionMessages errors) {
+		this.errors = errors;
+	}
+
+	public User getUser(SecuriteForm formulaire) throws Exception {
+		log.debug("********** Debut getUser AuthentificationBO **********");
+		try {
+			User user = userDAO.getUserByLoginEtPassword(
+					formulaire.getLoginUser(), formulaire.getPassword());
+			if (user == null) {
+				ActionError error = new ActionError("User.Incorrect");
+				errors.add("User.Incorrect", error);
+			}
+			return user;
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.fatal(e.getMessage());
+			return null;
+		} finally {
+			log.debug("********** Fin getUser AuthentificationBO **********");
+		}
+	}
+
+	@Override
+	public void create(User user) {
+		userDAO.create(user);
+	}
 
 }
